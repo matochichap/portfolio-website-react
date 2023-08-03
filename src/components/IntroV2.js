56b62ab1e2bb2152dd290123bf812134c4003045
Chan Rui Jia <ruijia.chan@gmail.com>
2023-08-03 23:20:41 +0700

    fix intro transition

--- src/components/IntroV2.js
+++ src/components/IntroV2.js
@@ -4,28 +4,27 @@ import { useEffect, useRef } from 'react';
 import { Link } from 'react-scroll';
 
 function Intro() {
-    const titleRef = useRef(null)
-    const subtitleRef = useRef(null)
+    const textRef = useRef(null)
     const btnRef = useRef(null)
     const ideRef = useRef(null)
-    const removeTransitionDelay = () => {
-        btnRef.current.style.transitionDelay = "0s"
+    const nextTransition = () => {
+        btnRef.current.classList.add("intro-animation")
+        ideRef.current.classList.add("intro-animation")
     }
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     entry.target.classList.add("intro-animation")
-                    // remove delay after title and subtitle transition finish playing
-                    setTimeout(removeTransitionDelay, 1000)
+                    // add next transiton after first plays
+                    setTimeout(nextTransition, 1000)
                 }
             })
         })
 
-        observer.observe(titleRef.current)
-        observer.observe(subtitleRef.current)
-        observer.observe(btnRef.current)
-        observer.observe(ideRef.current)
+        observer.observe(textRef.current)
+        // observer.observe(btnRef.current)
+        // observer.observe(ideRef.current)
 
         return () => {
             observer.disconnect()
@@ -37,12 +36,14 @@ function Intro() {
         <section id='intro' className='intro' style={{backgroundImage: `url("./resources/images/intro-background.jpg")`}}>
             <div className='intro-content'>
                 <div className='intro-content-left'>
-                    <h1 className='intro-title' ref={titleRef}>
-                        Rui Jia
-                    </h1>
-                    <h2 className='intro-subtitle' ref={subtitleRef}>
-                        Computer Science undergratuate at NUS
-                    </h2>
+                    <div className="intro-content-text" ref={textRef}>
+                        <h1 className='intro-title'>
+                            Rui Jia
+                        </h1>
+                        <h2 className='intro-subtitle'>
+                            Computer Science undergratuate at NUS
+                        </h2>
+                    </div>
                     <div className='intro-wrapper intro-link' ref={btnRef}>
                         <Link className='intro-linkx' to='projects' smooth={true} duration={500}>View Projects</Link>
                     </div>
@@ -52,20 +53,6 @@ function Intro() {
                 </div>
             </div>
         </section>
-        {/* <section id='intro' className='intro'>
-            <div>
-                <div className='intro-filler'></div>
-                <div className='intro-icon-box'>
-                    <img className='intro-icon' src="./resources/images/programming.png" alt="icon" />
-                </div>
-                <div className='intro-text-box'>
-                    <code className='intro-text'>
-                        {text}<Cursor></Cursor>
-                    </code>
-                </div>
-                <div className='intro-filler'></div>
-            </div>
-        </section> */}
         </>
     )
 }
